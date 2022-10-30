88d4d9f67422fbe8017dbc176b11bdb76704b3c9
Rustam1616 <data.rustam.nazarov@gmail.com>
2022-10-30 18:59:49 +0400

    lesson2-init

--- products/views.py
+++ products/views.py
@@ -3,7 +3,31 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Store'
+    }
+    return render(request, 'products/index.html', context)
 
 def products(request):
-    return render(request, 'products/products.html') 
+    context = {
+        'title': 'Store - Katalog'
+    }
+    return render(request, 'products/products.html', context) 
+
+
+def test_context(request):
+    context = {
+        'title': 'store',
+        'header': 'Dobro pojalovat',
+        'username': 'Ivan Ivanov',
+        'products': [
+            {'name': 'xudi adidas', 'price': '1000'},
+            {'name': 'c thenorthface', 'price': '15'},
+            {'name': 'over design', 'price': '1800'}
+        ],
+        #'promotion': True,
+        'products_of_promotion': [
+            {'name': 'ryukzak cherniy', 'price':'25'},
+        ]
+    }
+    return render(request, 'products/test-context.html', context)
